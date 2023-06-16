0d03e284c4f59d0aa90939bf8fbd35f471639900
CiksGg <kambarbekovamantur81@gmail.com>
2023-06-16 23:17:07 +0200

    onepiece

--- account/tasks.py
+++ account/tasks.py
@@ -6,6 +6,6 @@ from decouple import config
 @shared_task
 def send_activation_code(email, activation_code):
     activation_link = f'http://{config("SERVER_CONFIG")}/account/activate/{activation_code}/'
-    message = f'Активируйте аккаунт перейдя по ссылке {activation_link}\nВнимание! для регистрации с вашего баланса будет вычето 10$'
+    message = f'Активируйте аккаунт перейдя по ссылке {activation_link}\nВнимание! '
     send_mail('Activate account', message, 'admin@admin.com', [email])
     return 'Send'
